86d1f6f4931eea02e7284d3052d71c0340267c50
Geoffrey <geoffreyyang@gmail.com>
2021-07-16 19:57:11 -0400

    migrations done

---
+++ app/models/portfolio_stocks.py
@@ -0,0 +1,12 @@
+from .db import db
+
+
+class PortfolioStocks(db.Model):
+
+    __tablename__ = 'portfolio_stocks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    ticker = db.Column(db.VARCHAR(5), nullable=False)
+    basis = db.Column(db.Float, nullable=False)
+    share_count = db.Column(db.Integer, nullable=False, default=0)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
