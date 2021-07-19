61cf3a99bb84c5a22c2af4db22b1e04eccf1ac67
Chase Brashear <chase.brashear@gmail.com>
2021-07-19 15:31:56 -0700

    add graph functionality

--- react-app/src/components/Stock.js
+++ react-app/src/components/Stock.js
@@ -1,26 +1,79 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getSingleStock } from '../store/stocksStore';
+import { Line } from 'react-chartjs-2';
 
-function Stock() {
+function Stock({ ticker }) {
   const [stock, setStock] = useState({});
+  const [stockData, setStockData] = useState('dailyPrices')
+  const [labels, setLabels] = useState([1, 2, 3, 4, 5, 6])
+  const [data, setData] = useState({})
   const stocks = useSelector(state => state.stocks);
   const dispatch = useDispatch();
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: false,
+          },
+        },
+      ],
+    },
+  };
+
+  useEffect(() => {
+    dispatch(getSingleStock(ticker));
+    setStock(stocks[ticker])
+  }, [dispatch])
+
+  useEffect(() => {
+    if (stock) {
+      setData({
+        labels: stock[stockData],
+        datasets: [
+          {
+            label: ticker,
+            data: stock[stockData],
+            fill: false,
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgba(255, 99, 132, 0.2)',
+          },
+        ],
+      })
+    }
+  }, [stockData])
 
   return (
-    <>
-      <div>
+    <div className='graphContainer'>
+      <h1>{stock?.shortName}</h1>
+      <div className='graphButtons'>
         <button onClick={() => {
-          dispatch(getSingleStock('AAPL'));
-          setStock(stocks.AAPL);
-        }}>AAPL</button>
+          setStockData('dailyPrices');
+        }}>Today</button>
         <button onClick={() => {
-          dispatch(getSingleStock('TSLA'))
-          setStock(stocks.TSLA)
-        }}>TSLA</button>
-        <div>{stock?.ticker} - {stock?.currentPrice}</div>
+          setStockData('weeklyPrices');
+        }}>Weekly</button>
+        <button onClick={() => {
+          setStockData('oneMonthPrices');
+        }}>Monthly</button>
+        <button onClick={() => {
+          setStockData('yearlyPrices');
+        }}>Yearly</button>
+        <button onClick={() => {
+          setStockData('allTimePrices');
+        }}>All Time</button>
       </div>
-    </>
+      <div className='graph'>
+        {
+          (stock) ?
+          <Line data={data} options={options} />
+          :
+          <h3>Loading...</h3>
+        }
+        {/* <div>{stock?.ticker} - {stock?.currentPrice}</div> */}
+      </div>
+    </div>
   );
 }
 export default Stock;
