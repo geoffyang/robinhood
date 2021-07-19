941f75323a5b9ac3200c2f17a28e3aa6788e2de1
Chase Brashear <chase.brashear@gmail.com>
2021-07-19 12:24:51 -0700

    add working backend > store > thunks > frontend component for accessing external stock information

--- app/api/external_stocks.py
+++ app/api/external_stocks.py
@@ -10,6 +10,4 @@ external_stocks = Blueprint('external_stocks', __name__)
 def get_single_stock(ticker):
     httpResponse = requests.get("https://www.styvio.com/api/" + ticker)
     pythonData = httpResponse.json()
-    print(pythonData['ticker'] + " - " + pythonData['currentPrice'])
-    # return {'ticker': pythonData['ticker'], 'currentPrice': pythonData['currentPrice']}
     return pythonData
--- react-app/src/components/Stock.js
+++ react-app/src/components/Stock.js
@@ -1,29 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getSingleStock } from '../store/stocksStore';
-// import { useParams } from 'react-router-dom';
 
 function Stock() {
   const [stock, setStock] = useState({});
-  // const user = useSelector(state => state.session.user);
+  const stocks = useSelector(state => state.stocks);
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    (async() => {
-      const singleStock = await dispatch(getSingleStock('aapl'));
-      console.log('this is the singleStock: ', singleStock);
-      if(!singleStock) {
-        console.log('There should be error handling here!!!(Stock.js component)');
-      } else {
-        setStock(singleStock);
-      }
-    })();
-  }, [dispatch]);
 
   return (
     <>
       <div>
-        {stock.ticker} {stock.currentPrice}
+        <button onClick={() => {
+          dispatch(getSingleStock('AAPL'));
+          setStock(stocks.AAPL);
+        }}>AAPL</button>
+        <button onClick={() => {
+          dispatch(getSingleStock('TSLA'))
+          setStock(stocks.TSLA)
+        }}>TSLA</button>
+        <div>{stock?.ticker} - {stock?.currentPrice}</div>
       </div>
     </>
   );
