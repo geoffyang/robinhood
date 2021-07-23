73d61859e49d76eca6606d8e03c810a3e9fafa65
Nicholas Brancifort <brancifortnick@yahoo.com>
2021-07-23 13:36:40 -0400

    starting to style again, changed inputs back

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -43,50 +43,63 @@ const SignUpForm = () => {
   }
 
   return (
+    <div className='signup_form_container'>
     <form onSubmit={onSignUp}>
-      <div>
+      <div className='error_field'>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
+      <div className='row_one'>
+        <div className="user_name">
+          <input
+            type="text"
+            name="username"
+            onChange={updateUsername}
+            value={username}
+          ></input>
+        </div>
       </div>
-      <div>
-        <label>Email</label>
+      <div className='row_two'>
+        <div className="email_field">
         <input
-          type='text'
-          name='email'
+          type="text"
+          name="email"
           onChange={updateEmail}
           value={email}
+          placeholder='Email'
         ></input>
+        </div>
       </div>
-      <div>
-        <label>Password</label>
+      <div className='row_three'>
+        <div className='password_field'>
         <input
-          type='password'
-          name='password'
+          type="password"
+          name="password"
           onChange={updatePassword}
           value={password}
+          placeholder='Password (min. 10 characters)'
         ></input>
+       </div>
       </div>
-      <div>
-        <label>Repeat Password</label>
+      <div className='row_four'>
+        <div className='verify_password'>
         <input
-          type='password'
-          name='repeat_password'
+          type="password"
+          name="repeat_password"
           onChange={updateRepeatPassword}
           value={repeatPassword}
           required={true}
+          placeholder='Verify Password...'
         ></input>
+       </div>
       </div>
-      <button type='submit'>Sign Up</button>
+      <div className='row_five'>
+      <button
+      className='sign_up_button'
+      type="submit">Continue</button>
+      </div>
+      </div>
     </form>
   );
 };
